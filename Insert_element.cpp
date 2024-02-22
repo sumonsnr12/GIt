7a083e3766cec3cd41b2c987e063183cb7380264
Sumon Roy <124716976+sumonsnr12@users.noreply.github.com>
2024-02-22 18:45:59 +0600

    First element

---
+++ Insert_element.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N;
+    cin>>N;
+    int arr[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin>>arr[i];
+    }
+    int I = N;
+    int POS = 3 ,val = 10;
+    while (I>=POS)
+    {
+        arr[I+1] = arr[I];
+        I = I - 1;
+    }
+    N = N + 1;
+    arr[POS] = val;
+
+    for (int i = 0; i < N; i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+    
+    
+    
+}
